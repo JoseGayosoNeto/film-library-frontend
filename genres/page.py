091c50f83cbec404a8b1ed1679b4e660d012a0d3
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-04-29 09:59:24 -0300

    feat: add service and genre repository, logout service and genre integration with film-library-api API

--- genres/page.py
+++ genres/page.py
@@ -1,38 +1,35 @@
+from genres.service import GenreService
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
 
 
-genres = [
-    {
-        'id': 1,
-        'name': 'Ação'
-    },
-    {
-        'id': 2,
-        'name': 'Comédia'   
-    },
-    {
-        'id': 3,
-        'name': 'Terror'
-    },
-]
-
 def show_genres_page():
+    genre_services = GenreService()
     
-    st.write('Genres List')
-
-    AgGrid(
-        data = pd.DataFrame(genres),
-        key = 'genres_grid',
-        fit_columns_on_grid_load = True,
-    )
+    genres = genre_services.get_genres()
     
-    st.divider()
-    
+    if genres:
+        st.write('Genres List')
+        
+        genres_df = pd.json_normalize(genres)
+        AgGrid(
+            data = genres_df,
+            key = 'genres_grid',
+            fit_columns_on_grid_load = True,
+        )
+        
+        st.divider()
+    else:
+        st.warning('No genre found.')
+        
     st.title('Register New Genre')
     
     name = st.text_input('Genre Name', placeholder='Enter your genre here.')
     if st.button('Register'):
-        st.success(f'Genre {name} registered successfully.')
+        new_genre = genre_services.create_genre(name)
+        if new_genre:
+            st.rerun()
+        else:
+            st.error('Error when registering genre. Check the fields')
         
